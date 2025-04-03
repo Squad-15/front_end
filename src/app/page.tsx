2c12542e8f6a9ddd689185736da11093204b057a
Ryan <ryannarelli12@gmail.com>
2025-04-03 01:59:55 -0300

    removendo header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { Header } from "./components/Header";
 import { HeroPrinciple } from "./components/HeroPrinciple";
 
 export default function Home() {
     return (
+        <>
+        <Header />
         <HeroPrinciple />
+        </>
     );
 }
