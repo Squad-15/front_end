39fe7973eae7a0540611c972d1f3dfa6b9b6fa59
Ryan <ryannarelli12@gmail.com>
2025-04-19 15:25:09 -0300

    enviando ajustes

--- src/app/components/FormRegister/index.tsx
+++ src/app/components/FormRegister/index.tsx
@@ -27,85 +27,79 @@ export const FormRegister = () => {
         { id: 3, title: "Localidade e Permissão" },
       ];
 
-    return(
-      <div className="w-full max-w-4xl max-sm:max-w-lg mx-auto p-6 mt-6 md:mt-0 md:p-0">
-        <div className="text-center mb-12 sm:mb-16">
-            <a href="javascript:void(0)"><img
-            src="/assets/img/logo.png" alt="logo" className='w-44 inline-block' />
-            </a>
-            <h4 className="text-slate-600 text-h4 mt-6">Iniciar Novo Cadastro</h4>
-        </div>
-
-        <div className="flex justify-center gap-8 mb-8">
-            {steps.map(({ id, title }) => (
-            <StepIndicator key={id} step={id} currentStep={step} title={title} />
-        ))}
-    </div>
-
-      <form onSubmit={handleSubmit}>
-        {step === 1 && <StepOne formData={formData} handleChange={handleChange} />}
-
-        {step === 2 && (
-          <StepTwo
-              formData={formData}
-              handleChange={handleChange}
-              departaments={departaments}
-              profile={profile}
-              typeconnection={typeconnection}
-          />
-    )}      
-          {step === 3 && (
-          <StepThree
-              formData={formData}
-              handleChange={handleChange}
-              roleUser={roleUser}
-              optionsLocation={location}
-          />
+      return (
+        <>
+          {isSubmitting && (
+            <div className="fixed inset-0 bg-black bg-opacity-40 z-50 flex items-center justify-center">
+              <div className="w-16 h-16 border-4 border-white border-t-transparent rounded-full animate-spin" />
+            </div>
           )}
-          
-      <div className="mt-12 flex justify-between">
-            {step > 1 && (
-              <button
-                type="button"
-                onClick={handlePrevStep}
-                className="px-4 py-2 bg-gray-300 text-gray-800 rounded hover:bg-gray-400 transition"
-              >
-                Voltar
-              </button>
-            )}
-
-            {step < 3 ? (
-              <button
-                type="button"
-                onClick={handleNextStep}
-                className="px-4 py-2 bg-primary text-white rounded hover:bg-red-600 transition"
-              >
-                Próximo
-              </button>
-            ) : (
-              <button
-                type="submit"
-                className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700 transition flex items-center justify-center min-w-[120px]"
-                disabled={isSubmitting}
-              >
-                {isSubmitting ? (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-5 h-5 animate-spin fill-white"
-                    viewBox="0 0 24 24"
+      
+          <div className="w-full max-w-4xl max-sm:max-w-lg mx-auto p-6 mt-6 md:mt-0 md:p-0">
+            <div className="text-center mb-12 sm:mb-16">
+              <a href="javascript:void(0)">
+                <img src="/assets/img/logo.png" alt="logo" className="w-44 inline-block" />
+              </a>
+              <h4 className="text-slate-600 text-h4 mt-6">Iniciar Novo Cadastro</h4>
+            </div>
+      
+            <div className="flex justify-center gap-8 mb-8">
+              {steps.map(({ id, title }) => (
+                <StepIndicator key={id} step={id} currentStep={step} title={title} />
+              ))}
+            </div>
+      
+            <form onSubmit={handleSubmit}>
+              {step === 1 && <StepOne formData={formData} handleChange={handleChange} />}
+              {step === 2 && (
+                <StepTwo
+                  formData={formData}
+                  handleChange={handleChange}
+                  departaments={departaments}
+                  profile={profile}
+                  typeconnection={typeconnection}
+                />
+              )}
+              {step === 3 && (
+                <StepThree
+                  formData={formData}
+                  handleChange={handleChange}
+                  roleUser={roleUser}
+                  optionsLocation={location}
+                />
+              )}
+      
+              <div className="mt-12 flex justify-between">
+                {step > 1 && (
+                  <button
+                    type="button"
+                    onClick={handlePrevStep}
+                    className="px-4 py-2 bg-gray-300 text-gray-800 rounded hover:bg-gray-400 transition"
                   >
-                    <path
-                      d="M12 22c5.421 0 10-4.579 10-10h-2c0 4.337-3.663 8-8 8s-8-3.663-8-8c0-4.336 3.663-8 8-8V2C6.579 2 2 6.58 2 12c0 5.421 4.579 10 10 10z"
-                    />
-                  </svg>
-                ) : (
-                  "Cadastrar"
+                    Voltar
+                  </button>
                 )}
-            </button>
-
-            )}
+      
+                {step < 3 ? (
+                  <button
+                    type="button"
+                    onClick={handleNextStep}
+                    className="px-4 py-2 bg-primary text-white rounded hover:bg-red-600 transition"
+                  >
+                    Próximo
+                  </button>
+                ) : (
+                  <button
+                      type="submit"
+                      className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700 transition flex items-center justify-center min-w-[120px]"
+                      disabled={isSubmitting}
+                    >
+                      Cadastrar
+                  </button>
+                )}
+              </div>
+            </form>
           </div>
-      </form>
-    </div>
-    );
+        </>
+      );      
 }
