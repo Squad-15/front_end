fbcf4eabcc37fb38c0f2b26d70f72d2e9c11f7c1
Ryan <ryannarelli12@gmail.com>
2025-04-21 02:03:29 -0300

    style: Ajuste feito no background da tela de registro Foi realizado um ajuste no background, adicionando uma imagem de fundo. Também removi as bordas brancas de imagem de fundo, fazendo um edit

--- src/app/components/FormRegister/index.tsx
+++ src/app/components/FormRegister/index.tsx
@@ -35,7 +35,7 @@ export const FormRegister = () => {
             </div>
           )}
       
-          <div className="w-full max-w-4xl max-sm:max-w-lg mx-auto p-6 mt-6 md:mt-0 md:p-0">
+          {/* <div className="w-full max-w-4xl max-sm:max-w-lg mx-auto p-6 mt-6 md:mt-0 md:p-0">
             <div className="text-center mb-12 sm:mb-16">
               <a href="javascript:void(0)">
                 <img src="/assets/img/logo.png" alt="logo" className="w-44 inline-block" />
@@ -98,6 +98,72 @@ export const FormRegister = () => {
                 )}
               </div>
             </form>
+          </div> */}
+          <div className="w-full max-w-4xl max-sm:max-w-lg mx-auto p-6 mt-6 md:mt-0 md:p-0">
+            <div className="bg-white rounded-3xl shadow-lg p-9">
+              <div className="text-center mb-12 sm:mb-16">
+                <a href="javascript:void(0)">
+                  <img src="/assets/img/logo.png" alt="logo" className="w-44 inline-block" />
+                </a>
+                <h4 className="text-slate-600 text-h4 mt-6">Iniciar Novo Cadastro</h4>
+              </div>
+
+              <div className="flex justify-center gap-8 mb-8">
+                {steps.map(({ id, title }) => (
+                  <StepIndicator key={id} step={id} currentStep={step} title={title} />
+                ))}
+              </div>
+
+              <form onSubmit={handleSubmit}>
+                {step === 1 && <StepOne formData={formData} handleChange={handleChange} />}
+                {step === 2 && (
+                  <StepTwo
+                    formData={formData}
+                    handleChange={handleChange}
+                    departaments={departaments}
+                    profile={profile}
+                    typeconnection={typeconnection}
+                  />
+                )}
+                {step === 3 && (
+                  <StepThree
+                    formData={formData}
+                    handleChange={handleChange}
+                    roleUser={roleUser}
+                    optionsLocation={location}
+                  />
+                )}
+
+                <div className="mt-12 flex justify-between">
+                  {step > 1 && (
+                    <button
+                      type="button"
+                      onClick={handlePrevStep}
+                      className="px-4 py-2 bg-gray-300 text-gray-800 rounded hover:bg-gray-400 transition"
+                    >
+                      Voltar
+                    </button>
+                  )}
+
+                  {step < 3 ? (
+                    <button
+                      type="button"
+                      onClick={handleNextStep}
+                      className="px-4 py-2 bg-primary text-white rounded hover:bg-red-600 transition"
+                    >
+                      Próximo
+                    </button>
+                  ) : (
+                    <button
+                      type="submit"
+                      className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700 transition flex items-center justify-center min-w-[120px]"
+                    >
+                      Cadastrar
+                    </button>
+                  )}
+                </div>
+              </form>
+            </div>
           </div>
         </>
       );      
