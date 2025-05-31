f107559a164029c305a84f8a1c6e970168839395
Ryan <ryannarelli12@gmail.com>
2025-05-31 12:16:10 -0300

    enviando alterações

--- src/app/modulo/page.tsx
+++ src/app/modulo/page.tsx
@@ -7,7 +7,7 @@ import CardModulo from '../components/CardModulo'
 export default function LocationPage() {
   const cardsData = [
     {
-      image: 'https://sdmntprwestus3.oaiusercontent.com/files/00000000-fa18-61fd-bb84-fcd2eabdb0f9/raw?se=2025-05-22T20%3A03%3A48Z&sp=r&sv=2024-08-04&sr=b&scid=f0dcface-b5c6-5b5c-a0fb-bff51ec0f3b0&skoid=04233560-0ad7-493e-8bf0-1347c317d021&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-05-22T12%3A43%3A52Z&ske=2025-05-23T12%3A43%3A52Z&sks=b&skv=2024-08-04&sig=PZ3rGtNyfcni1qW%2BaeMS3CW71XRbtp07HUkjtCjVV8E%3D',
+      image: 'https://img.freepik.com/vetores-premium/ilustracao-vetorial-de-um-pequeno-trabalhador-em-tempo-de-brincadeira-de-construcao-animada_1080480-29487.jpg?w=2000',
       category: 'Aula 1',
       title: '300 Pontos',
       subTitle: 'Fundamentos da Construção Civil',
@@ -25,7 +25,7 @@ export default function LocationPage() {
       link: '#',
     },
     {
-      image: 'https://lh5.googleusercontent.com/OIQ2NIkdfwQT_sTUPUwz3wzodF1XLwH2rH31cbjVd_k8SMWxkW8eylNc_Vw8y0mplX7KHWMkSyqtor9OXidUzurRh9dyKHdPMSY2Z8AUhEdpcFW-phLWKulhybZwlshHgHipbdQ',
+      image: 'https://global-uploads.webflow.com/602a08aa6d8e750c207e189f/6032085e4ab0f228ae01dba3_avaliacao-de-desempenho-performance.png',
       category: 'Avaliação Final',
       title: '1000 Pontos',
       subTitle: 'Projeto Prático de Construção',
