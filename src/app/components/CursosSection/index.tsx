62b1d405284b5532a39657d0b526710b3413d4b6
Ryan <ryannarelli12@gmail.com>
2025-06-06 23:26:22 -0300

    enviando ajustes

--- src/app/components/CursosSection/index.tsx
+++ src/app/components/CursosSection/index.tsx
@@ -1,38 +1,36 @@
 'use client';
 
-import Image from 'next/image';
-
 const cursos = [
   {
     titulo: 'Gestão de Projetos',
     descricao: 'Aprenda estratégias eficazes para gerenciar projetos de construção com sucesso.',
-    imagem: '/images/gestao-projetos.jpg', // substitua pelo caminho real da imagem
+    imagem: 'https://images.sympla.com.br/5d7bacc89e827.jpg',
   },
   {
     titulo: 'Gestão de Obras e Custos',
     descricao: 'Aprenda controle de custos e otimização de recursos em projetos de construção.',
-    imagem: '/images/gestao-obras.jpg',
+    imagem: 'https://facilit.com.br/wp-content/uploads/2016/06/planejamento-estrategico-como-transformar-planos-em-acoes-efetivas25378.jpg',
   },
   {
     titulo: 'Liderança e Gestão de Equipes',
     descricao: 'Capacite líderes a gerenciar equipes de forma eficaz e motivar para alcançar resultados excepcionais.',
-    imagem: '/images/lideranca-equipes.jpg',
+    imagem: 'https://www.estrategiaparaacao.com.br/wp-content/uploads/2019/04/274822-como-envolver-o-funcionario-no-planejamento-estrategico-da-empresa.jpg',
   },
 
   {
   titulo: 'Segurança no Trabalho',
   descricao: 'Aprenda práticas essenciais para garantir a segurança no ambiente de trabalho e prevenir acidentes.',
-  imagem: '/images/seguranca-trabalho.jpg',
+  imagem: 'https://www.telavita.com.br/blog/wp-content/uploads/2022/12/planejamento-empresarial-1.jpg',
 },
 {
   titulo: 'Planejamento Estratégico',
   descricao: 'Desenvolva habilidades para criar, aplicar e avaliar planos estratégicos com foco em resultados.',
-  imagem: '/images/planejamento-estrategico.jpg',
+  imagem: 'https://www.slideteam.net/wp/wp-content/uploads/2023/09/5-Planejamento-estrategico.png',
 },
 {
   titulo: 'Comunicação Empresarial',
   descricao: 'Melhore sua comunicação interna e externa para fortalecer relacionamentos e a imagem da empresa.',
-  imagem: '/images/comunicacao-empresarial.jpg',
+  imagem: 'https://1.bp.blogspot.com/-LbFh6nG-bPg/W9vKocw-H2I/AAAAAAAAGOg/Gzbg_fxOzdsQOGwmeq7kSAh0KIJo_RmKQCLcBGAs/s1600/aprenda-a-fazer-um-planejamento-estrategico-para-sua-empresa-810x810.jpeg',
 },
 
 ];
@@ -49,11 +47,17 @@ export const CursosSection = () => {
             className="bg-black rounded-2xl overflow-hidden shadow-lg flex flex-col justify-between"
           >
             <div className="h-48 md:h-56 w-full relative">
-              <Image
+              {/* <Image
                 src={curso.imagem}
                 alt={curso.titulo}
                 fill
                 className="object-cover"
+              /> */}
+
+              <img
+                src={curso.imagem}
+                alt="Joana Costa"
+                className="absolute inset-0 w-full h-full object-cover"
               />
             </div>
             <div className="p-5 flex flex-col justify-between flex-1">
