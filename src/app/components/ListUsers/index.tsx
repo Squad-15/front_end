10d8d352a9a54116b26fb970fcc85dc234680308
Ryan <ryannarelli12@gmail.com>
2025-06-11 20:42:11 -0300

    enviando novos ajustes

--- src/app/components/ListUsers/index.tsx
+++ src/app/components/ListUsers/index.tsx
@@ -154,9 +154,20 @@ export const ListUsers = () => {
                         </td>
                         <td className="p-2 whitespace-nowrap">
                           <div className="flex justify-center space-x-2">
-                            <button className="text-blue-500 hover:text-blue-700" onClick={() => setIsModalOpenUserDetails(true)}>
+                            {/* <button className="text-blue-500 hover:text-blue-700" onClick={() => setIsModalOpenUserDetails(true)}>
+                              <IoEyeOutline size={18} />
+                            </button> */}
+
+                            <button
+                              className="text-blue-500 hover:text-blue-700"
+                              onClick={() => {
+                                setSelectedUserId(user.id);
+                                setIsModalOpenUserDetails(true);
+                              }}
+                            >
                               <IoEyeOutline size={18} />
                             </button>
+
                             <button
                                 className="text-yellow-500 hover:text-yellow-700"
                                 onClick={() => {
@@ -204,9 +215,9 @@ export const ListUsers = () => {
         </div>
       )}
 
-      {IsModalOpenUserDetails && (
+      {IsModalOpenUserDetails && selectedUserId && (
         <div className="fixed inset-0 p-4 flex flex-wrap justify-center items-center w-full h-full z-[1000] before:fixed before:inset-0 before:w-full before:h-full before:bg-[rgba(0,0,0,0.5)] overflow-auto">
-          <ModalDetailsUser closeModalUserDetails={() => setIsModalOpenUserDetails(false)} />
+          <ModalDetailsUser closeModalUserDetails={() => setIsModalOpenUserDetails(false)} userId={parseInt(selectedUserId)} />
         </div>
       )}
     </section>
